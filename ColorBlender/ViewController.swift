59730881de5aa60bc8a6bc3b0f03c711d2c08102
AnastasyiaMaximova <rsk.maximova@bk.ru>
2024-09-13 16:55:40 +0300

    prepare the project

--- ColorBlender/ViewController.swift
+++ ColorBlender/ViewController.swift
@@ -1,17 +1,13 @@
-//
-//  ViewController.swift
-//  ColorBlender
-//
-//  Created by Anastasya Maximova on 13.09.2024.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
