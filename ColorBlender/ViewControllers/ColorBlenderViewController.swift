827b9539d8a0078177d45779f31abdc7fdf457ee
AnastasyiaMaximova <rsk.maximova@bk.ru>
2024-11-18 22:37:38 +0300

    work with logic

--- ColorBlender/ViewControllers/ColorBlenderViewController.swift
+++ ColorBlender/ViewControllers/ColorBlenderViewController.swift
@@ -14,6 +14,16 @@ final class ColorBlenderViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    var delegate: ColorBlenderViewControllerDelegate?
+    
+    var redColor: CGFloat!
+    var greenColor: CGFloat!
+    var blueColor: CGFloat!
+    
     //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +31,13 @@ final class ColorBlenderViewController: UIViewController {
         setupSliders()
         setupValueLabels()
         setColor()
+        setupValueTF()
+        addDoneButtonOnKeyboard(textField: redTextField)
+        addDoneButtonOnKeyboard(textField: greenTextField)
+        addDoneButtonOnKeyboard(textField: blueTextField)
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
     }
     
     //MARK: - IB Actions
@@ -29,14 +46,23 @@ final class ColorBlenderViewController: UIViewController {
         switch sender {
         case redSlider:
             redValueLabel.text = String(format: "%.2F", redSlider.value)
+            redColor = CGFloat(redSlider.value)
+            redTextField.text = String(format: "%.2F", redSlider.value)
         case greenSlider:
             greenValueLabel.text = String(format: "%.2F", greenSlider.value)
+            greenColor = CGFloat(greenSlider.value)
+            greenTextField.text = String(format: "%.2F", greenSlider.value)
         default:
             blueValueLabel.text = String(format: "%.2F", blueSlider.value)
+            blueColor = CGFloat(blueSlider.value)
+            blueTextField.text = String(format: "%.2F", blueSlider.value)
         }
     }
     
-    @IBAction func DoneButtonTapped(_ sender: Any) {
+    @IBAction func DoneButtonTapped() {
+        view.endEditing(true)
+        delegate?.setViewColor(red: redColor, green: greenColor, blue: blueColor)
+        dismiss(animated: true)
     }
     
     //MARK: - Privet methods
@@ -55,15 +81,15 @@ extension ColorBlenderViewController {
     private func setupSliders() {
         redSlider.minimumTrackTintColor = .red
         redSlider.maximumTrackTintColor = .gray
-        redSlider.value = 0
+        redSlider.value = Float(redColor)
         
         greenSlider.minimumTrackTintColor = .green
         greenSlider.maximumTrackTintColor = .gray
-        greenSlider.value = 0
+        greenSlider.value = Float(greenColor)
         
         blueSlider.minimumTrackTintColor = .blue
         blueSlider.maximumTrackTintColor = .gray
-        blueSlider.value = 0
+        blueSlider.value = Float(blueColor)
     }
     
     private func setupValueLabels (){
@@ -71,5 +97,81 @@ extension ColorBlenderViewController {
         greenValueLabel.text = String(format: "%.2F", greenSlider.value)
         blueValueLabel.text = String(format: "%.2F", blueSlider.value)
     }
+    private func setupValueTF () {
+        redTextField.text = String(format: "%.2F", redSlider.value)
+        greenTextField.text = String(format: "%.2F", greenSlider.value)
+        blueTextField.text = String(format: "%.2F", blueSlider.value)
+    }
+}
+
+//MARK: - UITextFieldDelegate
+extension ColorBlenderViewController: UITextFieldDelegate {
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else {
+            showAlert(title: "Wrong format!",
+                      massage: "Please, enter correct value.",
+                      completion: {textField.text = ""}
+            )
+            return
+        }
+        
+        guard   let value = Double(text), 0...1 ~= value else {
+            showAlert(title: "Wrong format!",
+                      massage: "Please, enter correct value.",
+                      completion: {textField.text = ""}
+            )
+            return
+        }
+        switch textField {
+        case redTextField:
+            redColor = CGFloat(Double(textField.text!)!)
+            setupSliders()
+            setupValueLabels()
+        case greenTextField:
+            greenColor = CGFloat(Double(textField.text!)!)
+            setupSliders()
+            setupValueLabels()
+        default:
+            blueColor = CGFloat(Double(textField.text!)!)
+            setupSliders()
+            setupValueLabels()
+        }
+        setColor()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func addDoneButtonOnKeyboard (textField: UITextField) {
+        let keyboardToolBar: UIToolbar = UIToolbar()
+        keyboardToolBar.items = [
+            UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction(sender:))),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        ]
+        keyboardToolBar.sizeToFit()
+        textField.inputAccessoryView = keyboardToolBar
+    }
+    
+    @objc func doneButtonAction(sender: UIBarButtonItem) {
+        view.endEditing(true)
+     }
+}
+
+//MARK: - Show alert
+extension ColorBlenderViewController {
+    private func showAlert(title: String, massage: String, completion:(()->Void)? = nil){
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default){ _ in
+        completion?()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
--- ColorBlender/ViewControllers/ColoredViewController.swift
+++ ColorBlender/ViewControllers/ColoredViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
-final class ColoredViewController: UIViewController {
+protocol ColorBlenderViewControllerDelegate {
+    func setViewColor(red: CGFloat, green: CGFloat, blue: CGFloat)
+}
 
+final class ColoredViewController: UIViewController {
+    
+    private var red: CGFloat = 1
+    private var green: CGFloat = 1
+    private var blue: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setViewColor(red: red, green: green, blue: blue)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let colorBlenderVC = segue.destination as? ColorBlenderViewController
+        colorBlenderVC?.redColor = red
+        colorBlenderVC?.greenColor = green
+        colorBlenderVC?.blueColor = blue
+        colorBlenderVC?.delegate = self
     }
-    */
-
 }
+
+//MARK: - ColorBlenderViewControllerDelegate
+extension ColoredViewController: ColorBlenderViewControllerDelegate {
+    func setViewColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
+       view.backgroundColor = UIColor(
+            red: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue),
+            alpha: 1
+        )
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+}
+
